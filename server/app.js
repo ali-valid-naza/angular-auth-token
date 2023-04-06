b2c5ad3a575ca87881a014be95aecf7b15cdd723
OlegAnisimov <gramafon@yandex.ru>
2023-04-06 18:25:39 +0300

    server first structure

---
+++ server/app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+const authRouter = require('./routes/auth')
+const counterRouter = require('./routes/counter')
+
+app.use(express.static('./public'));
+app.use(express.json());
+
+app.use('/api/v1/login', authRouter);
+app.use('/api/v1/counter', counterRouter);
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
